fdd08cf2a19550e6f8ca6d019547359d55f95770
Sebastian Hoß <seb@xn--ho-hia.de>
2023-11-08 06:05:04 +0100

    switch to error-prone annotations to replace LGPL dependency
    fixes #49
    
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- storage-units-model/src/main/java/module-info.java
+++ storage-units-model/src/main/java/module-info.java
@@ -9,7 +9,7 @@
 module wtf.metio.storageunits.model {
 
     requires org.jetbrains.annotations;
-    requires com.github.spotbugs.annotations;
+    requires com.google.errorprone.annotations;
     requires org.jspecify;
 
     exports wtf.metio.storageunits.model;
